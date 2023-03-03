1745b13d202986d21a7308da93b4211e2f874d78
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-03-03 11:46:47 +0700

    chore: docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+console.log(process.env.STAGE);
 
 @Module({
   imports: [],
-  controllers: [AppController],
-
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
