8c969d342a399331e76cd4e5c5818e4fd9abb5c4
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-04-20 09:01:12 +0700

    feat: login

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,6 @@ import { CategoriesModule } from "./categories/categories.module";
 import { SuppliersModule } from "./suppliers/suppliers.module";
 import { ProductsModule } from "./products/products.module";
 import { S3Module } from "./s3/s3.module";
-import { LoggerModule } from "./config/logger.module";
 
 @Module({
   imports: [
@@ -18,7 +17,6 @@ import { LoggerModule } from "./config/logger.module";
       validationSchema: configValidationSchema,
       isGlobal: true,
     }),
-    LoggerModule,
     DatabaseModule,
     UsersModule,
     AuthModule,
