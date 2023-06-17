9f6447de44a519b7264734ebfd7508955376c191
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-06-17 15:40:27 +0700

    chore: change permission can accesss to address module

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -3,19 +3,20 @@ import { Request } from "express";
 
 import { CreateAddressDto } from "./dto/create-address.dto";
 import { JwtAuthGuard } from "src/auth/guards/jwt.guard";
-import { RolesGuard } from "../auth-manager/guards/role.guard";
 import { AddressService } from "./address.service";
 import { Roles } from "../auth-manager/decorators/roles.decorator";
 import { Role } from "src/shared";
 import { ReqUser } from "src/common/req-user.inter";
+import { UserGuard } from "src/auth/guards";
+import { User } from "src/auth/decorators";
 
 @Controller("address")
-@UseGuards(JwtAuthGuard, RolesGuard)
+@UseGuards(JwtAuthGuard, UserGuard)
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
   @Post()
-  @Roles(Role.Employee)
+  @User()
   async create(@Body() createAddressDto: CreateAddressDto, @Req() req: Request) {
     const user = req.user as ReqUser;
 
@@ -23,6 +24,7 @@ export class AddressController {
   }
 
   @Get()
+  @User()
   @Roles(Role.Employee)
   async getAll(@Req() req: Request) {
     const user = req.user as ReqUser;
@@ -31,7 +33,7 @@ export class AddressController {
   }
 
   @Delete(":id")
-  @Roles(Role.Employee)
+  @User()
   async delete(@Req() req: Request, @Param("id", new ParseUUIDPipe({ version: "4" })) id: string) {
     const user = req.user as ReqUser;
 
