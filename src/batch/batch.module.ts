1928973ca74f155407609d14ff88a7b552c622cc
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-06-22 11:26:56 +0700

    chore: a

--- src/batch/batch.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BatchService } from './batch.service';
-import { BatchController } from './batch.controller';
-
-@Module({
-  controllers: [BatchController],
-  providers: [BatchService]
-})
-export class BatchModule {}
--- src/batch/dto/update-batch.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBatchDto } from './create-batch.dto';
-
-export class UpdateBatchDto extends PartialType(CreateBatchDto) {}
