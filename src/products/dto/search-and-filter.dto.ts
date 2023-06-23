fbe6a2aa7d98d3f6a9709d050c494fef44e8796f
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-06-23 19:21:01 +0700

    feat: filter product

---
+++ src/products/dto/search-and-filter.dto.ts
@@ -0,0 +1,32 @@
+import { Transform } from "class-transformer";
+import { IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
+
+enum SortBy {
+  ASC = "asc",
+  DESC = "desc",
+  LATEST = "latest",
+  SALES = "sales",
+}
+
+export class SearchAndFilterQueryDto {
+  @IsString()
+  @IsOptional()
+  categories?: string;
+
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @IsOptional()
+  maxPrice?: number;
+
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @IsOptional()
+  minPrice?: number;
+
+  @IsEnum(SortBy)
+  @IsOptional()
+  orderBy?: SortBy;
+
+  @IsString()
+  keyword: string;
+}
