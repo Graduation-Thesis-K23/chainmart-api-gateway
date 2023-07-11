c49002547d58c452c540171b745223b09e1a0f22
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-07-11 15:42:15 +0700

    chore: a

--- src/batches/dto/update-batch.dto.ts
+++ src/batches/dto/update-batch.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBatchDto } from './create-batch.dto';
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateBatchDto } from "./create-batch.dto";
 
 export class UpdateBatchDto extends PartialType(CreateBatchDto) {}
