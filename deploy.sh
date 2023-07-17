0deae87d6ee5cefc5a6f11d43130b04d1d446f28
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-07-17 22:13:24 +0700

    feat: github action update version

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+echo "START EXECUTE SCRIPT"
+
+sudo chmod 777 /var/run/docker.sock
+sudo docker system prune -f -a
+
+sudo aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 376597958889.dkr.ecr.ap-southeast-1.amazonaws.com
+
+sudo docker compose stop
+sudo docker compose rm -f
+sudo docker compose pull
+sudo docker compose up -d
+
+echo "DONE EXECUTE SCRIPT"
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -5,21 +5,29 @@ import { ProductsService } from "./products.service";
 import { ProductsController } from "./products.controller";
 import { ErrorsModule } from "~/errors/errors.module";
 import { S3Module } from "~/s3/s3.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: "PRODUCT_SERVICE",
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: "product",
-            brokers: ["localhost:9092"],
-          },
-          consumer: {
-            groupId: "product-consumer",
-          },
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: async (configService) => {
+          console.log(configService.get("KAFKA_BROKERS").split(","));
+          return {
+            transport: Transport.KAFKA,
+            options: {
+              client: {
+                clientId: "product",
+                brokers: configService.get("KAFKA_BROKERS").split(","),
+              },
+              consumer: {
+                groupId: "product-consumer",
+              },
+            },
+          };
         },
       },
     ]),
