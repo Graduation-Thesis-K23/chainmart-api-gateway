cedc6c87e5465910f32b7fd83fd916475b5e159f
Hiep Nguyen <hiepnguyen6014@gmail.com>
2023-08-12 23:44:33 +0700

    feat: get remaining quantity

--- src/auth-manager/guards/jwt-employee.guards.ts
+++ src/auth-manager/guards/jwt-employee.guards.ts
@@ -1,5 +1,24 @@
-import { CanActivate, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
+import { Reflector } from "@nestjs/core";
 
 @Injectable()
-export class JwtEmployeeAuthGuard extends AuthGuard("jwt-employee") implements CanActivate {}
+export class JwtEmployeeAuthGuard extends AuthGuard("jwt-employee") implements CanActivate {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+}
